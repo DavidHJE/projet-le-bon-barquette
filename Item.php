c3f8c6f0af2bb159cbbd03337e87116128efe407
Dylan Domen <dylan.domen@gmail.com>
2020-11-19 08:09:05 +0100

    ajout/modifs des classes pour le projet : classe Item, class Drink, class Dessert, class Meal

--- Item.php
+++ Item.php
@@ -1,10 +1,79 @@
 <?php
 
-class Item{
+abstract class Item{
     public $name;
-    private $prix;
+    private $price;
+    private $description;
 
-    private function __construct(){
+    public function __toString(){
+        return $this->name."  ".$this->price."  ".$this->description."  ";
+    }
 
+    public function __construct($name,$price,$description){
+        $this->name = $name;
+        $this->price = $price;
+        $this->description =$description;
+    }
+
+
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of price
+     */ 
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set the value of price
+     *
+     * @return  self
+     */ 
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of description
+     */ 
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set the value of description
+     *
+     * @return  self
+     */ 
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
     }
 }
